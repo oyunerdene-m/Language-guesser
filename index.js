189a8de1e5e07b60426e882566da0579d0267ade
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-06-29 14:11:36 +0200

    error handling

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const langs = require('langs');
 const colors = require('colors');
 
 const input = franc(process.argv[2]);
-const res = langs.where('3', input).name;
-console.log(res.green);
+
+if (input === 'und') {
+	console.log('Sorry, make another example!!!'.red);
+} else {
+	const res = langs.where('3', input).name;
+	console.log(res.green);
+}
